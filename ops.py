97094af45fde3da6fe24d8dd11c53d5f07178487
zhuyue123 <zhu-yue@qq.com>
2018-10-29 09:41:31 +0800

    改变生成器鉴别器的结构

---
+++ ops.py
@@ -0,0 +1,146 @@
+import math
+import numpy as np 
+import tensorflow as tf
+
+from tensorflow.python.framework import ops
+
+from utils import *
+import tensorflow.contrib as tf_contrib
+
+
+# Xavier : tf_contrib.layers.xavier_initializer()
+# He : tf_contrib.layers.variance_scaling_initializer()
+# Normal : tf.random_normal_initializer(mean=0.0, stddev=0.02)
+
+weight_init = tf_contrib.layers.xavier_initializer()
+weight_regularizer = None
+
+
+image_summary = tf.summary.image
+scalar_summary = tf.summary.scalar
+histogram_summary = tf.summary.histogram
+merge_summary = tf.summary.merge
+SummaryWriter = tf.summary.FileWriter
+
+class batch_norm(object):
+  def __init__(self, epsilon=1e-5, momentum = 0.9, name="batch_norm"):
+    with tf.variable_scope(name):
+      self.epsilon  = epsilon
+      self.momentum = momentum
+      self.name = name
+
+  def __call__(self, x, train=True):
+    return tf.contrib.layers.batch_norm(x,
+                      decay=self.momentum, 
+                      updates_collections=None,
+                      epsilon=self.epsilon,
+                      scale=True,
+                      is_training=train,
+                      scope=self.name)
+'''
+def conv_cond_concat(x, y):
+  """Concatenate conditioning vector on feature map axis."""
+  x_shapes = x.get_shape()
+  y_shapes = y.get_shape()
+  return concat([
+   y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]]),x], 3)
+'''
+def conv2d(input_, output_dim, 
+       k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+       name="conv2d"):
+  with tf.variable_scope(name):
+    w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
+              initializer=tf.truncated_normal_initializer(stddev=stddev))
+    conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
+
+    biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
+    conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
+
+    return conv
+
+def deconv2d(input_, output_shape,
+       k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
+       name="deconv2d", with_w=False):
+  with tf.variable_scope(name):
+    # filter : [height, width, output_channels, in_channels]
+    w = tf.get_variable('w', [k_h, k_w, output_shape[-1], input_.get_shape()[-1]],
+              initializer=tf.random_normal_initializer(stddev=stddev))
+    
+    try:
+      deconv = tf.nn.conv2d_transpose(input_, w, output_shape=output_shape,
+                strides=[1, d_h, d_w, 1])
+
+    # Support for verisons of TensorFlow before 0.7.0
+    except AttributeError:
+      deconv = tf.nn.deconv2d(input_, w, output_shape=output_shape,
+                strides=[1, d_h, d_w, 1])
+
+    biases = tf.get_variable('biases', [output_shape[-1]], initializer=tf.constant_initializer(0.0))
+    deconv = tf.reshape(tf.nn.bias_add(deconv, biases), deconv.get_shape())
+
+    if with_w:
+      return deconv, w, biases
+    else:
+      return deconv
+
+def conv(x, channels, kernel=4, stride=2, pad=0, pad_type='zero', use_bias=True, sn=False, scope='conv_0'):
+    with tf.variable_scope(scope):
+        if pad_type == 'zero' :
+            x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]])
+        if pad_type == 'reflect' :
+            x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]], mode='REFLECT')	
+        x = tf.layers.conv2d(inputs=x, filters=channels,kernel_size=kernel, 
+			     kernel_initializer=weight_init,
+                             kernel_regularizer=weight_regularizer,
+                             strides=stride, 
+			     use_bias=use_bias)
+        return x
+			
+def lrelu(x, leak=0.2, name="lrelu"):
+  return tf.maximum(x, leak*x)
+
+def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False):
+  shape = input_.get_shape().as_list()
+
+  with tf.variable_scope(scope or "Linear"):
+    matrix = tf.get_variable("Matrix", [shape[1], output_size], tf.float32,
+                 tf.random_normal_initializer(stddev=stddev))
+    bias = tf.get_variable("bias", [output_size],
+      initializer=tf.constant_initializer(bias_start))
+    if with_w:
+      return tf.matmul(input_, matrix) + bias, matrix, bias
+    else:
+      return tf.matmul(input_, matrix) + bias
+def spectral_norm(w, iteration=1):
+    w_shape = w.shape.as_list()
+    w = tf.reshape(w, [-1, w_shape[-1]])
+
+    u = tf.get_variable("u", [1, w_shape[-1]], initializer=tf.truncated_normal_initializer(), trainable=False)
+
+    u_hat = u
+    v_hat = None
+    for i in range(iteration):
+        """
+        power iteration
+        Usually iteration = 1 will be enough
+        """
+        v_ = tf.matmul(u_hat, tf.transpose(w))
+        v_hat = l2_norm(v_)
+
+        u_ = tf.matmul(v_hat, w)
+        u_hat = l2_norm(u_)
+
+    sigma = tf.matmul(tf.matmul(v_hat, w), tf.transpose(u_hat))
+    w_norm = w / sigma
+
+    with tf.control_dependencies([u.assign(u_hat)]):
+        w_norm = tf.reshape(w_norm, w_shape)
+        w_norm = w / sigma
+
+    return w_norm
+########################################
+def l2_norm(v, eps=1e-12):
+    return v / (tf.reduce_sum(v ** 2) ** 0.5 + eps)
+
+def relu(x):
+    return tf.nn.relu(x)
